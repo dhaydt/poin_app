c2f2a9807005972af9b672a4987b56a19631d711
dhaydt <1992dayat@gmail.com>
2023-05-28 16:25:40 +0700

    fix broadsa[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C

--- resources/views/filament/pages/broadcast-notif.blade.php
+++ resources/views/filament/pages/broadcast-notif.blade.php
@@ -238,7 +238,7 @@
               <path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="currentColor"></path>
             </svg>
 
-            <span x-show="! isUploadingFile" class="">
+            <span class="">
               Kirim Broadcast!
             </span>
           </span>
