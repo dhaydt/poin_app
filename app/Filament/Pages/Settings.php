a0cc5f195f1ec14f0a0f0ce962605ac56d6ad798
dhaydt <1992dayat@gmail.com>
2023-06-27 13:59:31 +0700

    hide nav settings

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -8,5 +8,7 @@ class Settings extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
+    protected static bool $shouldRegisterNavigation = false;
+
     protected static string $view = 'filament.pages.settings';
 }
